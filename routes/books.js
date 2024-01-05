4475dd2b48f435ad5b1d6ebc0573522f262bbf20
Jie <jies_2408@126.com>
2024-01-05 18:23:27 +1100

    Books Module

--- routes/books.js
+++ routes/books.js
@@ -1,19 +1,99 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer'); //used for file format
+const path = require('path'); //used for file path
+const fs = require('fs'); //file system used to delete files
+const Book = require('../models/book');
+const uploadPath = path.join('public', Book.coverImageBasePath) //set the path of the file
+const imageMimeTypes = ['image/jpeg', 'image/png', 'image/jpg'] //set the file type
+const Author = require('../models/author');
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {  //set upload destination
+        callback(null, uploadPath)
+    },
+    filename: (req, file, callback) => {
+        console.log(file);
+        callback(null, Date.now()+path.extname(file.originalname)) //set the file name
+    }
+})
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, callback) => {
+        callback(null, imageMimeTypes.includes(file.mimetype));
+    }
+})
 
 // All Books Route
-router.get('/', (req, res) => {
-    res.render('books/index')
+router.get('/', async (req, res) => {
+    let query = Book.find()
+    if(req.query.title!=null && req.query.title!=''){  //query in url
+        query = query.regex('title', new RegExp(req.query.title, 'i'))
+    }
+    if(req.query.publishedBefore!=null && req.query.publishedBefore!=''){ 
+        query = query.lte('publishDate', req.query.publishedBefore)
+    }
+    if(req.query.publisherAfter!=null && req.query.publisherAfter!=''){ 
+        query = query.gte('publishDate', req.query.publisherAfter)
+    }
+    try{
+        const books = await query.exec()
+        res.render('books/index', {books: books, searchOptions: req.query})
+    }catch{
+        redirect('/')
+    }
 })
 
 //New Book Route
-router.get('/new', (req, res) => {
-    res.render('books/new')
+router.get('/new', async (req, res) => {
+    const book = new Book()
+    renderNewPage(res, book, hasError = false)
 })
 
 //Create Book Route
-router.post('/', (req, res) => {
-    res.send('Create')
+router.post('/', upload.single('cover'), async (req, res) => {
+    // console.log('req.file： '+req.files)
+    const fileName = req.file!=null?req.file.filename:null  //get the file name
+    const book = new Book({
+        title: req.body.title,
+        author: req.body.author,
+        description:req.body.description,
+        publishDate: new Date(req.body.publishDate),
+        pageCount: req.body.pageCount,
+        coverImageName: fileName
+    })
+
+    try{
+        await book.save()
+        res.redirect('books')
+    }catch(error){
+        console.log(error+' in creating book')
+        removeBookCover(book.coverImageName) //delete the file if book is not created
+        renderNewPage(res, book, true)
+    }
 })
+
+function removeBookCover(fileName){
+    fs.unlink(path.join(uploadPath, fileName), err => {
+        if(err) console.error(err) // don't worry about sending it to users
+    })
+}
+ 
+//encapculate the renderNewPage function
+async function renderNewPage(res, book, hasError = false){
+    try{
+        const authors = await Author.find({})
+        const params = {
+            authors: authors,
+            book: book
+        }
+        if(hasError) {
+            params.errorMessage = 'Error Creating Book'
+        }
+        res.render('books/new', params)
+    }catch(error){
+        res.redirect('/books')
+    }
+}
 
 module.exports = router 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const Book = require('../models/book');
 const router = express.Router();
 
 // Home page
-router.get('/', (req, res) => {
-    res.render('index')
+router.get('/', async (req, res) => {
+    let books = []
+    try{
+        books = await Book.find().sort({createAt: 'desc'}).limit(10).exec()
+    }catch{
+        books = []
+    }
+    res.render('index', {books:books})
 })
 
 module.exports = router 
